63a51ac5853a8ccb98d213e8b0c83d8cb0fb9b69
premalshrestha <premalshrestha00@gmail.com>
2026-08-12 06:48:18 +0545

    Commit remaining changes

---
+++ Session2/completed/students/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("students/", views.student_list, name="student-list"),
+    path("create/", views.student_create, name="student-create"),
+    path("<int:student_id>/edit/", views.student_update, name="student-update"),
+    path("<int:student_id>/delete/", views.student_delete, name="student-delete"),
+]
---
+++ Session2/completed/students/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render,redirect
+from .models import Student
+from .forms import StudentForm
+from django.shortcuts import get_object_or_404
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def student_list(request):
+    students = Student.objects.all()
+
+    return render(
+        request,
+        "students/list.html",
+        {"students": students}
+    )
+
+def student_create(request):
+
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("student-list")
+
+    else:
+        form = StudentForm()
+
+    return render(
+        request,
+        "students/student_form.html",
+        {"form": form}
+    )
+
+def student_update(request, student_id):
+
+    student = get_object_or_404(
+        Student,
+        id=student_id
+    )
+
+    if request.method == "POST":
+        form = StudentForm(
+            request.POST,
+            instance=student
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("student-list")
+
+    else:
+        form = StudentForm(instance=student)
+
+    return render(
+        request,
+        "students/student_form.html",
+        {"form": form}
+    )
+def student_delete(request, student_id):
+
+    student = get_object_or_404(
+        Student,
+        id=student_id
+    )
+
+    if request.method == "POST":
+        student.delete()
+        return redirect("student-list")
+
+    return render(
+        request,
+        "students/student_confirm_delete.html",
+        {"student": student}
+    )
